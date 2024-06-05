cef7f9b7f4c078731655e4f8e2ba639c57512b67
matt <javascript.12.03.2024@gmail.com>
2024-06-05 10:10:39 +0100

    update dwitter.js

--- js/dwitter.js
+++ js/dwitter.js
@@ -73,7 +73,7 @@
                       
                       console.log(1);
                       try{
-                            debugger;
+                            //debugger;
                             u(t);
                       }
                       catch(err){
