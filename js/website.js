0958f38d5c6ba5b6c6c4f5280d3413865621c1f9
matt <javascript.12.03.2024@gmail.com>
2024-06-09 10:55:59 +0100

    update website.js

--- js/website.js
+++ js/website.js
@@ -1,7 +1,7 @@
 
         var hdr;
         var background;
-        var contents;
+        var content;
         var center
         var ftr;
         
@@ -13,21 +13,41 @@
         
         function initdom(){   //c
         
+              var winw                      = window.innerWidth;
+              
               hdr                           = $('#hdr');
               var h1                        = hdr.offsetHeight;
+              
               ftr                           = $('#ftr');
               var h2                        = ftr.offsetHeight;
+              
               background                    = $('#background');
               background.style.top          = h1+'px';
               background.style.height       = window.innerHeight-h1-h2+'px';
-              center                        = $('#center');
-              var l                         = center.offsetLeft;
-              var t                         = center.offsetTop;
               
-              contents                      = $('#contents');
-              contents.style.top            = t+'px';
-              var w                         = contents.offsetWidth;
-              contents.style.left           = l-w-25+'px';
+              center                        = $('#center');
+              var l4                        = center.offsetLeft;
+              var t4                        = center.offsetTop;
+              var w4                        = center.offsetWidth;
+              
+              content                       = $('#content');
+              if(winw<1200){
+                    content.style.display   = 'none';
+              }else{
+                    content.style.top            = t4+'px';
+                    var w5                       = content.offsetWidth;
+                    if(w4+w5+20>winw){
+                          var tw                    = winw-w4-20;
+                          content.style.left        = '10px';
+                          content.style.width       = tw+'px';
+                          center.style.marginLeft   = tw+20+'px';
+                    }else{
+                          var tl                    = winw-w4-w5-20;
+                          content.style.left        = tl+'px';
+                          var tl                    = t1+w5+20;
+                          center.style.marginLeft   = t1+'px';
+                    }
+              }
               
               
               $.all('.code').forEach(node=>{
