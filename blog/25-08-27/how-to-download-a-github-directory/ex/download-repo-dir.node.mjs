1add825d87ce6fb79a8aa7ec805c282a6bd94435
matt <javascript.12.03.2024@gmail.com>
2025-10-13 21:05:51 +0100

    save file

--- blog/25-08-27/how-to-download-a-github-directory/ex/download-repo-dir.node.mjs
+++ blog/25-08-27/how-to-download-a-github-directory/ex/download-repo-dir.node.mjs
@@ -7,21 +7,24 @@
         var owner   = 'javascript-2020';
         var repo    = 'stackoverflow';
         var branch  = 'main';
-        var path    = '';
+        var path    = '/auto-login/';
         
+        //https://github.com/javascript-2020/stackoverflow/tree/main/auto-login
+                                                                                console.log(owner,repo,branch);
         if(path[0]=='/')path    = path.slice(1);
         if(path && path.slice(-1)!='/')path  += '/';
-        
+                                                                                console.log(path);
         var file    = `${path.split('/').filter(Boolean).at(-1)||repo}/`;
         fs.mkdirSync(file);    
         var url     = `https://api.github.com/repos/${owner}/${repo}/git/trees/${branch}?recursive=true`;
         var json    = await fetch(url).then(res=>res.json());
         
-        json.tree.forEach(async item=>{
+        json.tree.forEach(async(item,i)=>{
         
               if(!item.path.startsWith(path))return;
-              
+                                                                                //console.log(item);
               var fn    = item.path.slice(path.length);
+                                                                                console.log(fn,item.size);
               if(item.type=='tree'){
                     fs.mkdirSync(file+fn);
               }else{
