4ed9570752354afcb3d46005f3bcadf9e614fb1a
matt <javascript.12.03.2024@gmail.com>
2025-10-18 22:40:36 +0100

    save file

--- blog/25-07-25/http-streaming-download/ex/test.js
+++ blog/25-07-25/http-streaming-download/ex/test.js
@@ -1,17 +1,4 @@
       
-      <h3>test stream</h3>
-      
-      <style>
-        #output   {white-space:pre;font-family:monospace;font-size:16px;padding:0 50px}
-      </style>
-      
-      <div id=output>
-      </div>
-      
-      <script>
-            
-            output.textContent    = '';
-            var log   = (...args)=>((output.textContent+=args.join(' ')+'\n') && console.log.apply(console,args));
             
             (async()=>{
             
@@ -19,27 +6,15 @@
                     
                     var stream    = new TextDecoderStream();
                     var reader    = res.body.pipeThrough(stream).getReader();
-                                                                                      log('response stream aquired');
+                                                                                      console.log('response stream aquired');
                     while(true){
                     
                           const {value,done}    = await reader.read();
                           if(done)break;
-                                                                                      log('fetch',value);                                                                          
+                                                                                      console.log('fetch',value);                                                                          
                     }//while
                     
                     console.log('done');
                     
             })();
       
-      </script>
-      
-      <script>
-      
-            window.onmessage    = e=>{
-            
-                  console.log('rec',e.data);
-                  
-            };
-            
-            
-      </script>
