85c24e076d5f194cfee65ec89e6bd5cba7dda4a4
matt <javascript.12.03.2024@gmail.com>
2025-10-18 21:51:15 +0100

    save file

--- blog/25-07-25/http-streaming-download/ex/test.js
+++ blog/25-07-25/http-streaming-download/ex/test.js
@@ -1,17 +1,29 @@
-      <script>
       
+      <h3>test stream</h3>
+      
+      <style>
+        #output   {white-space:pre;font-family:monospace;font-size:16px}
+      </style>
+      
+      <div id=output>
+      </div>
+      
+      <script>
+            
+            var log   = (...args)=>(output.textContent+=args.join(' ')+'\n' && console.log.apply(console,args));
+            
             (async()=>{
             
                     var res       = await fetch('/stream');
                     
                     var stream    = new TextDecoderStream();
                     var reader    = res.body.pipeThrough(stream).getReader();
-                                                                                      console.log('response stream aquired');
+                                                                                      log('response stream aquired');
                     while(true){
                     
                           const {value,done}    = await reader.read();
                           if(done)break;
-                                                                                      console.log('fetch',value);                                                                          
+                                                                                      log('fetch',value);                                                                          
                     }//while
                     
                     console.log('done');
