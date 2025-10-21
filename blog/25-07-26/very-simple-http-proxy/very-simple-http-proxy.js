6f79c698cbd90cd1b8530179cdd63a9afc77b9bd
matt <javascript.12.03.2024@gmail.com>
2025-10-21 22:03:57 +0100

    save file

--- blog/25-07-26/very-simple-http-proxy/very-simple-http-proxy.js
+++ blog/25-07-26/very-simple-http-proxy/very-simple-http-proxy.js
@@ -7,9 +7,14 @@
         var cert;
         setup();
 
+        var mode    = 'http';
+
         
-        //https.createServer({key,cert},request).listen(8005);
-        http.createServer(request).listen(8005);
+        if(mode=='https'){
+              https.createServer({key,cert},request).listen(8005);
+        }else{
+              http.createServer(request).listen(8005);
+        }
                                                                                 console.log('listening 8005');
         function request(req,res){
                                                                                 console.log('8005 :',req.url);
@@ -18,22 +23,37 @@
                     res.end(html);
                     return;
               }
+         
+              var url   = `${mode}://localhost:8006${req.url}`;
+                                                                                console.log('8005 :',url);
+              if(mode=='http'){                                                                                
+                    var req2   = http.request(url,{ca:cert,method:req.method},result);
+              else{
+                    var req2   = https.request(url,{ca:cert,method:req.method,rejectUnauthorized:false},result);
+              }
               
-              var req2   = http.request('http://localhost:8006'+req.url,{ca:cert,method:req.method},res2=>{
+              function result(res2){
                 
                     res2.on('data',data=>res.write(data));
                     res2.on('end',()=>res.end());
                     
-              });
+              }//result
               
               req.on('data',data=>req2.write(data));
-              req.on('end',()=>req2.end());
+              req.on('end',()=>{
+                                                                                console.log('8005 :','req end'); 
+                    req2.end()
+                    
+              });
               
         }//request
+
         
-        
-        //https.createServer({key,cert},request2).listen(8006);
-        http.createServer(request2).listen(8006);
+        if(mode=='https'){
+              https.createServer({key,cert},request2).listen(8006);
+        }else{
+              http.createServer(request2).listen(8006);
+        }
                                                                               console.log('listening 8006');
         function request2(req,res){
                                                                               console.log('8006 :',req.url);
@@ -71,55 +91,57 @@ function setup(){
 
         key   = `
 -----BEGIN PRIVATE KEY-----
-MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCa8SM60MEabNel
-k9rwWp2Rg0C0UNQEqRLnKWWVKZt/s0/Jj6B9bRKMFjTWwWQwgupEV2TEfy8ULCCv
-WMut2NzeLHGny/YO6DffkxELk11gnl50xaLTCXdNZ9YJ8lH6j1Es2RobMIOR+Dr9
-v5ViXtoV0DP7wsR4LssppANcQ8FdjL4s2miJsOtw0zNuOieBTstA4cHZ1PzEISfU
-DuF0uo0YEopBg1AzccGISTgtzBiRxHaMVdKCmhH5eJ9U9hVnfefFXYsPC/xO3M9j
-4VNs1GM5PcvGrzUf+BIAObPqtDOPZCuF7LTjwGdQEMeA1HZz7M8zH4be2lsdG9vT
-RieEHhMnAgMBAAECggEACz58pAQKP3SuoaF9PfwVez3fMDa9bCko6gWjDZQbaMUG
-gUYfC3si4W+rHfqi3rJhddwxv+DK9dfN92umPFOEk8r95IN0s9g+VcdCcWicWiqk
-CC1fh5kkQ0XJzqb7NAe6iFqo/lgi9DDjdw60NJNVmEenvhmAUIh0zezNIaD5I2S5
-mQQyeMxaOV1Cggu0/ljhBmyZsfyC0vrkxy/OR2LKAuFpurd5wkqa62AIfZBzNW0T
-mCVNgR3SA+2Oc5lpA8jHJH/e+UEtjCXSCEuj/DpfUwrYTCQn4tddCDMRCPpHOuqm
-3p+DitV/QGQNfKAKe/NuBzwEgZjXY2SrVfcI859MIQKBgQD9V8i8Bj1qAK1vPa17
-8k21UWV723CC9O9XX+QnPzoe7bCI0iUR3OhpbOD0ZrYLCCUC4ICZd8cQhAqG1RWC
-Cdzve6UZ2y3+1JL5ISX0Vkyd1/VJ3Eho9hHNLUhw2PY5sTn6gCApR5nPJ3J2Gx8B
-6jSMsd+D5DRPdoDRjtM79WEsRQKBgQCckSaWEWba6dLhSyDaYozajFtTNBm4fbdR
-T4iFt7R1nIbKTzj4RKV79Ox4cSDG0N/GEWjVp9HCtdR0KHhVgiJtWKE3p7GzjliT
-ZtA9uPXRJU+l7S0YAXZQ55tJvc8wfdJABogtLduOJyYTWjceM0dVF9rU0QWWufrr
-jU9z6052ewKBgCkAvzVnUSNJ7sgVVHG+GsOg4+ry7jfTyrT6iSOtAQwmbi8GzAcj
-GN6PR61mvkn0zIC1szqjfy12IXoHHM8KJ57/BQ61QbF/2DXIHqt4GDhnzoRgybtr
-MCBkFthk1R0rSPX5LLthokw71zO0WgVmUZD/7Owc0DaYOGsyUY/fvBV9AoGAVONK
-JYFn3/d489f1BxL5xaDl/xW3gFUBKJ/TqJRyznXAiz4q6Ejn5G5mK+NCXyluuohD
-69uimkxV1Wj/3XTCT8EHIBLXAUCDe9/5tg1rkisFpTQUB/j6+mCozHGSPXtqnHYu
-LTi42rkVJtb6HXC2E0fsyQhPXGqXxlEGrBX/o7MCgYBmXg48QgHOjnoCsLsrcBsH
-xHwMAmUFE7XzB8L4sfc8bRI0RmqdHZfh8Qa0FcmFJfRfgpKwfXg6WFC2krGUZmcS
-7x/5Rp1Gxj3c58/q/X4hRXDeCbXZ1Nn/jm9JMbADXlLLlGHopQ7h1qxE0yyq6toc
-Gxs6VHKB4HcBOtYjuKXzIg==
------END PRIVATE KEY-----`;
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCJqjhlLiDSk0Im
+cGgALaVtxbZ+jG/359e2AkXJH/3Y0q5eJB3UKr6vANr1D/Jg947Nsk1gcvGd8m5n
++lO/YMSjEOf4eAwoTolBeIo9NPkz1iYE5O/1f/Kz/Z5lc7axIPPEZ+yiVxh3lUyF
+XUQr0jZJG3b9EkiyzomwaQHzj+X+0+0d1hm3ZtTB95YW/cELkVb8uqIpcf3VXaah
+gtMhcvXzMuDtJJuzFrKC7PEzO0pEXEXrPebAv3FrElXllsooJ7aEjWYCV80HfQP5
+2tlirx76iayr1mciMaGP0apDG0qc61aOid9Pn+5mEOM8iYZV/tly/TKSxpx7FuZT
+p+USyBILAgMBAAECggEAQ5GoAomElLawpjtP4J6G7v03GKAHqQ9DglNVO2WXa9/k
+s9JYTbX5R95MmjIMjmmTzwRwFCH208aVSDXG+00c1sjKgtsxZGs/1UG74FTq5/V5
+Tbesy7vUPWIDZ9pG1mPNIh68N+Y13SQLnf3sIvIXsQRo/8gEpLbZQBwaWBjOE6fk
+XvUNbHW5zTIUEawK8ZCDOsinWXnwHd0uuioESLEuf+s69VOmwMyNhmXqtGzIqrOs
+Fj5X7KnG6vXUbWvVjargUHsgjAQmQ4AmcyKsWw23SvByVeQDnJgZ1y3sirC+F2G6
+146ZkP5Bo91BO0+D4t93mK2arNBPMow5/9wk0sJQMQKBgQD1tefbQVhmr3tl4ect
+Tx/EVrV7VutEJVCKBap4mzIhsvA0grssoH5sumAYQuWWcBA1ce+hZcfsOIKPwklg
+S0Rhw1GCgCkadkNk1nLFbmjlu5Vhjc2iX+TQPPIwztntqHU4WbBzvPalwgpsFdp/
+Yw3OOwOtSo8RgT9TGoo6N/qv6QKBgQCPbggK1JwWrlAWnWw8tJfMi0PChVGjPQqB
+L2OhlHaebVbfgS0ZoH/y7Z4BSyEIWCZ6aunVb3RP+S7eXImgoxD7IbX+x/LhJJzT
+BnXr+Ri46RGN9TJS3lUC7TafMjzSEBIcMM+TGQ6raYwabjNTvr9EkPLhZeTrAQJh
+G6L3XkBN0wKBgARJCuUpC5im4NkTCowgwCYVeQ7QokPK/s+Zb7KwyjwKOeRAj3Ek
+z2+oBHsH16c7Bx1Xgl1nQAhfp+9H7S2vwl6OIILNJ2pdu6krkE7fMRcooV9VMMvv
+LjdviK3PPb/GW10DO2+ZURfs/W+LJgdou9nn75V7Immkvmz9noGLbmTZAoGARvaQ
+ohMpGwupclb2214akMgCgXaGurYTwXDZ4XLb8Wx1g+Wck8ZWJ7Iq78blFATRHNZS
+1Rfsuzi1WKz+Ju0nFKlMKz9wSrZbpQjYRSUcdzhRSCQdfuSjuOv2j3NCpkfvPeVk
+fgnyuRpiiB0n5fJFHb59fMl2JlN8guEykyMdu6MCgYEAjgdMweIv35bbBybSpiFI
+ZUlRbaNGi2iog3mnR+uCu+Cc0UOfN2KcmfbtrLuDaEfouE9BoiedLzadgRgO2s6t
+Gh0smyi4dJsUFg3s8fAVUaO7s+tk37RS2hOlItjHohTlPvK6KnS5eP12MuNQ5sEP
+M+X5Mi2Er2HxEJ/JsA8zCKY=
+-----END PRIVATE KEY-----
+`;
 
         cert    = `
 -----BEGIN CERTIFICATE-----
-MIIDcjCCAlqgAwIBAgIBATANBgkqhkiG9w0BAQUFADAlMSMwIQYDVQQDExpsb2Nh
+MIIDeDCCAmCgAwIBAgIBATANBgkqhkiG9w0BAQUFADAlMSMwIQYDVQQDExpsb2Nh
 bGhvc3QgdGVzdCBjZXJ0aWZpY2F0ZTAeFw0yNTEwMjExNzUxMDBaFw0yNjEwMjEx
 NzUxMDBaMCUxIzAhBgNVBAMTGmxvY2FsaG9zdCB0ZXN0IGNlcnRpZmljYXRlMIIB
-IjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmvEjOtDBGmzXpZPa8FqdkYNA
-tFDUBKkS5ylllSmbf7NPyY+gfW0SjBY01sFkMILqRFdkxH8vFCwgr1jLrdjc3ixx
-p8v2Dug335MRC5NdYJ5edMWi0wl3TWfWCfJR+o9RLNkaGzCDkfg6/b+VYl7aFdAz
-+8LEeC7LKaQDXEPBXYy+LNpoibDrcNMzbjongU7LQOHB2dT8xCEn1A7hdLqNGBKK
-QYNQM3HBiEk4LcwYkcR2jFXSgpoR+XifVPYVZ33nxV2LDwv8TtzPY+FTbNRjOT3L
-xq81H/gSADmz6rQzj2Qrhey048BnUBDHgNR2c+zPMx+G3tpbHRvb00YnhB4TJwID
-AQABo4GsMIGpMAkGA1UdEwQCMAAwIAYDVR0RBBkwF4IJbG9jYWxob3N0hwR/AAAB
-hwR/AAACMB0GA1UdDgQWBBTFTq3s81JbUmUg+u94ZQ7kHOAzZjALBgNVHQ8EBAMC
-AvQwOwYDVR0lBDQwMgYIKwYBBQUHAwIGCCsGAQUFBwMDBggrBgEFBQcDBAYIKwYB
-BQUHAwEGCCsGAQUFBwMIMBEGCWCGSAGG+EIBAQQEAwIA5zANBgkqhkiG9w0BAQUF
-AAOCAQEAk0B0HgWBtnmuCjlI81SpbUYbhp6UUm3oqvs5bvJLeVdcomy4ym/P2lHC
-YJhPVaTmruAmB+2Q60b9KSLvHKalxZzf6W//zCROvh5VbZI2WH7FFIuH7u2yGvew
-Lmgn6YU6BCc98bvR1h6tVYUa2M4uRCbz7ch2Cd43qVmOFGO4GcZX85QUiglkCYee
-rnezXV68vTU+eK31leuQCVQc5Efp2NjLzR6SjzyujjIPm5dUKNZnKQrzyWGlhXLS
-NyN5dLsPb8EocwT7GIdywFI3T0e8dITTMG82lqzpYUUy9fh417f2MAEbdG+vDHa4
-dahJ5GrzvRUijuPmZVg20O7j8tGsWA==
------END CERTIFICATE-----`;
+IjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiao4ZS4g0pNCJnBoAC2lbcW2
+foxv9+fXtgJFyR/92NKuXiQd1Cq+rwDa9Q/yYPeOzbJNYHLxnfJuZ/pTv2DEoxDn
++HgMKE6JQXiKPTT5M9YmBOTv9X/ys/2eZXO2sSDzxGfsolcYd5VMhV1EK9I2SRt2
+/RJIss6JsGkB84/l/tPtHdYZt2bUwfeWFv3BC5FW/LqiKXH91V2moYLTIXL18zLg
+7SSbsxayguzxMztKRFxF6z3mwL9xaxJV5ZbKKCe2hI1mAlfNB30D+drZYq8e+oms
+q9ZnIjGhj9GqQxtKnOtWjonfT5/uZhDjPImGVf7Zcv0yksacexbmU6flEsgSCwID
+AQABo4GyMIGvMA8GA1UdEwQIMAYBAf8CAQAwIAYDVR0RBBkwF4IJbG9jYWxob3N0
+hwR/AAABhwR/AAACMB0GA1UdDgQWBBTXCn5rSAXVxTEFoxIBAIxsYLbMfDALBgNV
+HQ8EBAMCAvQwOwYDVR0lBDQwMgYIKwYBBQUHAwIGCCsGAQUFBwMDBggrBgEFBQcD
+BAYIKwYBBQUHAwEGCCsGAQUFBwMIMBEGCWCGSAGG+EIBAQQEAwIA5zANBgkqhkiG
+9w0BAQUFAAOCAQEAQ4n9m2MjntHK4607P/ZxcdM+lFd9Y4ulysApLYrUOPqnQNXV
+u6pCCRyp/GJhrSyjfBhjHRg+TfOViWfpNV4MN5+d6fIQ2Rlm4APcNlsaply4KN5y
+1litt6Z25Nq6p18pmGF6OZZVbtSM82BIzctniR9NTDopFfzXr+Bw2pSo2cx58x4p
+npnftL/P3wX+pzBalF8H8Xh4hkiWwxTIe1tWojWlkT16fa8Y7d2D+/mD9l++oWHD
+bQbvzESakd4nVh97+LV0FefUQ3P4BaEdOcPXm+kuwgIp/rQln9n0he3ZUFd7UKNF
++/Aig6NrbNLnZM6aB5loq8EHcCkXQhw+wrKqHA==
+-----END CERTIFICATE-----
+`;
         
 }        
