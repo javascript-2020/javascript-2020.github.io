3f3bfba220b72204f3384570ff4647994c5c0857
matt <javascript.12.03.2024@gmail.com>
2025-10-21 09:25:30 +0100

    save file

---
+++ code-dev/25-10-21/override-createserver/ex/test.js
@@ -0,0 +1,20 @@
+      
+            
+            (async()=>{
+            
+                    var res       = await fetch('/stream');
+                    
+                    var stream    = new TextDecoderStream();
+                    var reader    = res.body.pipeThrough(stream).getReader();
+                                                                                      console.log('response stream aquired');
+                    while(true){
+                    
+                          const {value,done}    = await reader.read();
+                          if(done)break;
+                                                                                      console.log('fetch',value);                                                                          
+                    }//while
+                    
+                    console.log('done');
+                    
+            })();
+      
