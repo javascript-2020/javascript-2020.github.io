9947d6bffcac81be6f32eaad380b3fe007b83161
matt <javascript.12.03.2024@gmail.com>
2025-10-23 08:17:20 +0100

    save file

---
+++ blog/25-07-31/fetch-download-stream/ex/ex.js
@@ -0,0 +1,17 @@
+
+
+        var res       = await fetch(url,{headers,method,body});
+        
+        var decoder   = new TextDecoderStream();
+        var stream    = res.body.pipeThrough(decoder)
+        var reader    = stream.getReader();
+
+        while(true){
+        
+              const {value,done}    = await reader.read();
+              if(done)break;
+                                                                          console.log('fetch',value);
+                                                                          
+        }//while
+        
+        console.log('done');        
