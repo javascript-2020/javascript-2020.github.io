9fa9f492bab696ce5fb0cf880fad8177e569ae68
matt <javascript.12.03.2024@gmail.com>
2025-11-01 15:39:04 +0000

    save file

--- utils/gcloud/generate-token-from-service-account-keyfile/generate.js
+++ utils/gcloud/generate-token-from-service-account-keyfile/generate.js
@@ -3,85 +3,23 @@
 
         async function generate(keyfile,scope){
               
-
-
+              var encoder           = new TextEncoder();
+              
               var clientEmail       = keyfile.client_email;
-              var privateKeyPem     = keyfile.private_key.replace(/\\n/g,'\n');
+              var privateKeyPem     = keyfile.private_key;
+              
+              privateKeyPem         = norm_key(privateKeyPem);
               
               var assertion         = await buildJwtAssertion({clientEmail,privateKeyPem,scope});
-              var json              = await exchangeForAccessToken(assertion);
-              var token             = json.access_token;
-
+              var {json,error}      = await exchangeForAccessToken(assertion);
+              if(error){
+                    return {error};
+              }
               
+              var token             = json.access_token;
               return {json,token};
               
       
-            
-              function base64url(input){
-              
-                    let bytes;
-                    
-                    if(typeof input=='string'){
-                          bytes   = encoder.encode(input);
-                    }else if(input instanceof ArrayBuffer){
-                          bytes   = new Uint8Array(input);
-                    }else if(ArrayBuffer.isView(input)){
-                          bytes   = new Uint8Array(input.buffer, input.byteOffset, input.byteLength);
-                    }else{
-                          throw new TypeError('Unsupported input');
-                    }
-                    
-                    var bin   = '';
-                    for(var i=0;i<bytes.length;i++){
-                      
-                          bin  += String.fromCharCode(bytes[i]);
-                          
-                    }//for
-                    
-                    var str   = btoa(bin).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '');
-                    return str;
-                
-              }//base64url
-      
-            
-              function pemToArrayBuffer(pem){
-              
-                    const b64   = pem.replace(/-----BEGIN [^-]+-----/g, '')
-                                        .replace(/-----END [^-]+-----/g, '')
-                                        .replace(/\s+/g, '');
-                    const raw   = atob(b64);
-                    const buf   = new Uint8Array(raw.length);
-                    
-                    for(let i=0;i<raw.length;i++){
-                      
-                          buf[i]    = raw.charCodeAt(i);
-                            
-                    }//fpr
-                    
-                    return buf.buffer;
-                    
-              }//pem_buf
-      
-            
-              async function importPkcs8PrivateKey(pem) {
-              
-                    var keyData   = pemToArrayBuffer(pem);
-                    var key       = await crypto.subtle.importKey('pkcs8',keyData,{name:'RSASSA-PKCS1-v1_5',hash:'SHA-256'},false,['sign']);
-                    return key
-                    
-              }//import
-      
-            
-              async function signRS256(privateKey, dataToSign) {
-              
-                    var dataBytes   = encoder.encode(dataToSign);
-                    var sig         = await crypto.subtle.sign({name:'RSASSA-PKCS1-v1_5'},privateKey,dataBytes);
-                    var uint8       = new Uint8Array(sig);
-                    return uint8;
-                    
-              }//sign
-      
-            
               async function buildJwtAssertion({clientEmail,privateKeyPem,scope,aud}){
               
                     scope                 = Array.isArray(scope) ? scope.join(' ') : scope;
@@ -95,20 +33,23 @@
                     var payload           = {iss,scope,aud,iat,exp};
                 
                     var header            = {alg:'RS256',typ:'JWT'};
-                    var encodedHeader     = base64url(JSON.stringify(header));
-                    var encodedPayload    = base64url(JSON.stringify(payload));
+                    var str               = JSON.stringify(header);
+                    var encodedHeader     = b64url(str);
+                    
+                    var str               = JSON.stringify(payload);
+                    var encodedPayload    = b64url(str);
                     var unsigned          = `${encodedHeader}.${encodedPayload}`;
                 
                     var key               = await importPkcs8PrivateKey(privateKeyPem);
                     var sig               = await signRS256(key,unsigned);
-                    var encodedSig        = base64url(sig);
+                    var encodedSig        = b64url(sig);
                     var str               = `${unsigned}.${encodedSig}`;
                     return str
                     
               }//build
             
               
-              async function exchangeForAccessToken(assertion) {
+              async function exchangeForAccessToken(assertion){
               
                     var url       = 'https://oauth2.googleapis.com/token';
                     var body      = new URLSearchParams({grant_type:'urn:ietf:params:oauth:grant-type:jwt-bearer',assertion});
@@ -127,20 +68,119 @@
                     }//catch
                     if(err){
                                                                                 console.error(err);
-                          disp(err.toString());
-                          return;
+                          var error   = err.toString();
+                          return {error}
                     }
                     
                     if(!res.ok){
-                          throw new Error(`Token exchange failed: ${res.status} ${res.statusText} ${await res.text()}`);
+                          var error   = `Token exchange failed: ${res.status} ${res.statusText} ${await res.text()}`;
+                          return {error};
                     }
                                                                                 // { access_token, token_type, expires_in }
                     var json    = await res.json();
-                    return json;
+                    return {json};
                     
               }//exchange
+
+
+              async function importPkcs8PrivateKey(pem){
+              
+                    var buf       = pem_buf(pem);
+                    var params    = {name:'RSASSA-PKCS1-v1_5',hash:'SHA-256'};
+                    var key       = await crypto.subtle.importKey('pkcs8',buf,params,false,['sign']);
+                    return key
+                    
+              }//import
+      
             
-        
+              async function signRS256(private,data){
+              
+                    var bytes       = encoder.encode(data);
+                    var sig         = await crypto.subtle.sign({name:'RSASSA-PKCS1-v1_5'},private,bytes);
+                    var uint8       = new Uint8Array(sig);
+                    return uint8;
+                    
+              }//sign
+
+            
+              function norm_key(pem){
+                
+                    pem   = pem.replace(/\\n/g,'\n');
+                    return pem;
+                
+              }//norm
+              
+              
+              function b64url(input){
+              
+                    var uint8   = _uint8(input);
+                    if(!uint8){
+                          throw new TypeError('Unsupported input');
+                    }
+                    
+                    var bin   = '';
+                    var n     = uint8.length;
+                    for(var i=0;i<n;i++){
+                      
+                          var byte    = uint8[i];
+                          bin        += String.fromCharCode(byte);
+                          
+                    }//for
+                    
+                    var b64   = btoa(bin);
+                    b64       = b64.replaceAll('+','-');
+                    b64       = b64.replaceAll('/','_');
+                    b64       = b64.replaceAll('=','');
+                    
+                    var url   = b64;
+                    return url;
+                
+              }//b64url
+
+      
+              function _uint8(input){
+                    
+                    var uint8;
+                    if(typeof input=='string'){
+                          uint8   = encoder.encode(input);
+                    }
+                    if(input instanceof ArrayBuffer){
+                          uint8   = new Uint8Array(input);
+                    }
+                    if(ArrayBuffer.isView(input)){
+                          uint8   = new Uint8Array(input.buffer,input.byteOffset,input.byteLength);
+                    }
+                    return uint8;
+                    
+              }//to_uint8
+
+              
+              function pem_buf(pem){
+              
+                    var i1    = pem.indexOf('\n');
+                    var i2    = pem.lastIndexOf('\n');
+                    var s     = pem.slice(i1+1,i2);
+                    var b64   = pem.replaceAll('\n','');
+                    /*
+                    var b64   = pem.replace(/-----BEGIN [^-]+-----/g,'')
+                                        .replace(/-----END [^-]+-----/g,'')
+                                        .replace(/\s+/g,'');
+                    */
+                    var bin     = atob(b64);
+                    var n       = bin.length;
+                    var uint8   = new Uint8Array(n);
+                    
+                    for(var i=0;i<n;i++){
+                      
+                          uint8[i]    = bin.charCodeAt(i);
+                            
+                    }//fpr
+                    
+                    var buf   = uint8.buffer;
+                    return buf;
+                    
+              }//pem_buf
+      
         }//generate
 
 
