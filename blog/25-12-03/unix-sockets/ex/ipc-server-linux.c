d91e81dd74c1c475e892b1d278ab21628a506aad
matt <javascript.12.03.2024@gmail.com>
2025-12-04 04:40:55 +0000

    save file

--- blog/25-12-03/unix-sockets/ex/ipc-server-linux.c
+++ blog/25-12-03/unix-sockets/ex/ipc-server-linux.c
@@ -7,10 +7,11 @@
         #include <stdio.h>
         #include <unistd.h>
         #include <string.h>
+
         
-        int main() {
+        int main(){
           
-              int sock    = socket(AF_UNIX, SOCK_STREAM, 0);
+              int sock    = socket(AF_UNIX,SOCK_STREAM,0);
               if(sock<0){
                     perror("socket");
                     return 1;
@@ -26,8 +27,8 @@
               strcpy(addr.sun_path,"/tmp/mysock");
           
               if(bind(sock,(struct sockaddr*)&addr,sizeof(addr))<0){
-                      perror("bind");
-                      return 1;
+                    perror("bind");
+                    return 1;
               }          
               listen(sock,1);
               
