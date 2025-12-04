56dbaebc669f70647d28c82c39ea67649b211dd5
matt <javascript.12.03.2024@gmail.com>
2025-12-04 04:38:15 +0000

    save file

--- blog/25-12-03/unix-sockets/ex/ipc-server-linux.node.js
+++ blog/25-12-03/unix-sockets/ex/ipc-server-linux.node.js
@@ -1,5 +1,34 @@
-
-
-//  ipc-server-linux.node.js
+        
+        
+        
+// server.js
+        
+        var net         = require('net');
+        var PIPE_PATH   = '/tmp/mysock';
+        
+                                                                                // Clean up old socket file if it exists
+        var fs          = require('fs');
+        
+        if(fs.existsSync(PIPE_PATH)){
+              fs.unlinkSync(PIPE_PATH);
+        }
+        
+        var server    = net.createServer(con=>{
+                                                                                console.log('Client connected');
+              con.on('data',data=>{
+                                                                                console.log('Received from client:', data.toString());
+              });
+        
+              con.on('end',()=>{
+                                                                                console.log('Client disconnected');
+              });
+                                                                                // Send a test message
+              con.write('Hello from Node server over Unix socket!');
+              
+        });
+        
+        server.listen(PIPE_PATH,()=>{
+                                                                                console.log('Server listening on', PIPE_PATH);
+        });
 
 
