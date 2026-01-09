6c686632d6589d3602f5bceaec9a68caf4b498ee
matt <javascript.12.03.2024@gmail.com>
2026-01-09 14:41:44 +0000

    save file

--- blog/25-08-07/determine-datatypes-in-javascript/ex/ex2.js
+++ blog/25-08-07/determine-datatypes-in-javascript/ex/ex2.js
@@ -1,7 +1,9 @@
 
       var myObject                    = {};
       myObject[Symbol.toStringTag]    = 'my-object';
-
-      var type    = datatype(myObject);
       
+      var datatype    = v=>Object.prototype.toString.call(v).slice(8,-1).toLowerCase();
+      
+      var type        = datatype(myObject);
       console.log(type);
+      
