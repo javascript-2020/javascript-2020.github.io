b6537c2d91e981c18e619dee3f45f8e823bfd2b3
matt <javascript.12.03.2024@gmail.com>
2026-01-18 08:24:53 +0000

    save file

--- blog/25-07-24/http-server/ex/http-server-minimal.js
+++ blog/25-07-24/http-server/ex/http-server-minimal.js
@@ -6,33 +6,27 @@
                                                                                 console.log(`http://localhost:${port}/`);
         function request(req,res){
                                                                                 console.log(req.method,req.url);
-              var url   = req.url.slice(1);
-              
-              if(url==''){
-                    res.writeHead(200,{'content-type':'text/html'}).end(html);
-                    return;
-              }
-              
-              var ext     = url.split('.').at(-1),type;
-              switch(ext){
-              
-                case 'html'   : type='text/html';
-                case 'js'     : type='text/javascript';       break;
-                case 'css'    : type='text/css';              break;
-                
-              }//switch
+              var path    = req.url.slice(1);
+              var type    = mime(path);
               
               res.writeHead(200,{'content-type':type});
               require('fs').createReadStream(url).pipe(res);
               
         }//request
         
+        function mime(path){
         
-        var html    = `
-              <style>html{font-family:arial}body{margin:20px}</style>
-              <h3>It Works!</h3>
-        `;
-        
+              var ext     = path.split('.').at(-1),type;
+              switch(ext){
+              
+                case 'html'   : return 'text/html';
+                case 'js'     : return 'text/javascript';
+                case 'css'    : return 'text/css';
+                
+              }//switch
+              return 'application/octet-stream';
+              
+        }//mime
         
         
         
