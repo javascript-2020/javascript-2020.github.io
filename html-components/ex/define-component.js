9ca0e716ed0f352cfe1014cdc655e5c61ab6adbc
matt <javascript.12.03.2024@gmail.com>
2026-01-20 07:43:09 +0000

    save file

---
+++ html-components/ex/define-component.js
@@ -0,0 +1,33 @@
+
+
+(function({mod,host}){
+
+  var obj   = {};
+  
+  
+        obj.initmod   = function(params){
+        }//initmod
+        
+        
+  //vars:
+  
+  
+  //:
+  
+  
+        obj.init    = async function(){
+        }//init
+        
+        
+  //:
+  
+  
+        obj.initdom   = async function(){
+        }//initdom
+        
+        
+        
+  return obj;
+  
+})
+
