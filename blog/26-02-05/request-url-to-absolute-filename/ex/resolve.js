1ac2b04015472a0c43673061f5a1f3517db6f859
matt <javascript.12.03.2024@gmail.com>
2026-02-18 11:48:20 +0000

    save file

---
+++ blog/26-02-05/request-url-to-absolute-filename/ex/resolve.js
@@ -0,0 +1,49 @@
+
+/*
+  requires
+    path
+*/
+
+        resolve.df    = false;
+        
+        async function resolve(url,docroot='.'){
+                                                                                resolve.df && console.log('=== resolve v2.0 ===');
+                                                                                resolve.df && console.log('url :',url);
+                                                                                resolve.df && console.log('docroot :',docroot);
+              var err;
+              try{
+              
+                    url         = decodeURI(url);
+                    
+              }//try
+              catch(err2){
+              
+                    err   = err2;
+                    
+              }//catch
+              if(err){
+                                                                                resolve.df && console.error(err);
+                    return false;
+              }
+              
+              url         = url.slice(1);
+                                                                                resolve.df && console.log('url :',url);
+              var root    = path.resolve(docroot);
+              root       += path.sep;
+                                                                                resolve.df && console.log('root :',root);
+              var abs     = path.resolve(docroot,url);
+                                                                                resolve.df && console.log('abs :',abs);
+                                                                                
+              if(!abs.startsWith(root)){
+                                                                                resolve.df && console.log('fail');
+                    return false;
+              }
+              
+              if(url.endsWith('/')){
+                    abs  += '/';
+              }
+                                                                                resolve.df && console.log('ok',abs);
+              return abs;
+              
+        }//resolve
+        
