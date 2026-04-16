78f64ff70029c3893fc31ff449aec5bcd36568d5
matt <javascript.12.03.2024@gmail.com>
2026-04-16 12:05:34 +0100

    save file

--- blog/26-04-16/node-gyp-always-on-top/ex/always-on-top-min.js
+++ blog/26-04-16/node-gyp-always-on-top/ex/always-on-top-min.js
@@ -2,7 +2,7 @@
 
 
         const addon   = require('./always-on-top.node');
-                                                                                console.log("Setting active window always on top");
+                                                                                console.log('Setting active window always on top');
         addon.setAlwaysOnTop();
         
         
