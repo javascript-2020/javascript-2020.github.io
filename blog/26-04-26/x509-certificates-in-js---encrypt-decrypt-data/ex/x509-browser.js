a566da22988689a4de5545b91f665d094943d20f
matt <javascript.12.03.2024@gmail.com>
2026-04-27 20:08:09 +0100

    save file

--- blog/26-04-26/x509-certificates-in-js---encrypt-decrypt-data/ex/x509-browser.js
+++ blog/26-04-26/x509-certificates-in-js---encrypt-decrypt-data/ex/x509-browser.js
@@ -67,8 +67,8 @@
               var b64         = pem.replace(/-----BEGIN PRIVATE KEY-----/, '')
                                   .replace(/-----END PRIVATE KEY-----/, '')
                                   .replace(/\s+/g, '');
-              var der_uint8   = b64_uint8(b64);
-              var buf         = der_uint8.buffer;
+              var uint8       = b64_uint8(b64);
+              var buf         = uint8.buffer;
               var priv_key    = await crypto.subtle.importKey('pkcs8',buf,{name:'RSA-OAEP',hash:'SHA-256',},true,['decrypt']);
               return priv_key;
               
