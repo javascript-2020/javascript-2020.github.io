66c9eddbed89aac905946216fee29fa3d23238dc
matt <javascript.12.03.2024@gmail.com>
2026-05-16 03:52:32 +0100

    save file

--- blog/26-04-26/x509-certificates-in-js---encrypt-decrypt-data/ex/aes-encrypt-decrypt-nodejs.js
+++ blog/26-04-26/x509-certificates-in-js---encrypt-decrypt-data/ex/aes-encrypt-decrypt-nodejs.js
@@ -13,53 +13,81 @@ aes-encrypt-decrypt-nodejs.js
 
 
 
-const crypto = require("crypto");
-
-function importAesKey(base64) {
-  return Buffer.from(base64, "base64"); // raw 32‑byte key
-}
-
-
-function aesEncryptNode(keyBuf, plaintext) {
-  const iv = crypto.randomBytes(12);
+        var crypto    = require('crypto');
+        
+        
+        
+        function importAesKey(base64){
+        
+              return Buffer.from(base64,'base64'); // raw 32‑byte key
+              
+        }//importAesKey
+        
+        
+        function aesEncryptNode(keyBuf,plaintext,iv_bits=96){
+        
+              var bytes       = iv_bits/8;
+              var iv          = crypto.randomBytes(bytes);
+              
+              var cipher      = crypto.createCipheriv('aes-256-gcm',keyBuf,iv);
+              
+              var buf1        = cipher.update(plaintext,'utf8');
+              var buf2        = cipher.final();
+              
+              var encrypted   = Buffer.concat([buf1,buf2]);
+              
+              var tag         = cipher.getAuthTag();
+              
+              var buf         = Buffer.concat([ciphertext,authTag]);
+              
+              var blob        = iv_buf_blob(iv,buf);
+              return blob;
+              
+        }//aesEncryptNode
+        
+        
+        
+        function aesDecryptNode(keyBuf,encrypted){
+        
+              var iv = Buffer.from(encrypted.iv, 'base64');
+              var data = Buffer.from(encrypted.data, 'base64');
+              var tag = Buffer.from(encrypted.tag, 'base64');
+              
+              var decipher = crypto.createDecipheriv('aes-256-gcm', keyBuf, iv);
+              decipher.setAuthTag(tag);
+              
+              var decrypted = Buffer.concat([
+                decipher.update(data),
+                decipher.final()
+              ]);
+              
+              return decrypted.toString('utf8');
+              
+        }//aesDecryptNode
+        
+        
+  //:
   
-  const cipher = crypto.createCipheriv("aes-256-gcm", keyBuf, iv);
   
-  const encrypted = Buffer.concat([
-    cipher.update(plaintext, "utf8"),
-    cipher.final()
-  ]);
-  
-  const tag = cipher.getAuthTag();
-  
-  return {
-    iv: iv.toString("base64"),
-    data: encrypted.toString("base64"),
-    tag: tag.toString("base64"),
-  };
-}
-
-
-
-function aesDecryptNode(keyBuf, encrypted) {
-  const iv = Buffer.from(encrypted.iv, "base64");
-  const data = Buffer.from(encrypted.data, "base64");
-  const tag = Buffer.from(encrypted.tag, "base64");
-  
-  const decipher = crypto.createDecipheriv("aes-256-gcm", keyBuf, iv);
-  decipher.setAuthTag(tag);
-  
-  const decrypted = Buffer.concat([
-    decipher.update(data),
-    decipher.final()
-  ]);
-  
-  return decrypted.toString("utf8");
-}
-
-
-
-
-
-
-
+        function iv_buf_blob(iv,buf){
+        
+              var n1      = iv.length;
+              var n       = n1+buf.length;
+              var uint8   = new Uint8Array(n);
+              uint8.set(iv,0);
+              uint8.set(buf,n1);
+              var blob    = new Blob([uint8]);
+              return blob;
+              
+        }//iv_buf_blob
+        
+        
+        
+        
+        async function blob_buffer(){
+        
+              var buf       = await blob.arrayBuffer();
+              var buffer    = Buffer.from(buf);
+              return buffer;
+              
+        }//blob_buffer
